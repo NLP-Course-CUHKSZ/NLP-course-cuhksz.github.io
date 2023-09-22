75a9deafce8c5706c8e82198c3f19a0ced7b4d21
wangxidong <wangxidong06@gmail.com>
2023-09-22 13:23:19 +0800

    update gpt

---
+++ Assignment1_tmp/code/gen.py
@@ -0,0 +1,42 @@
+import requests
+import json
+
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+class GPT35:
+    def __init__(self,api_key,organization = None) -> None:
+        self.api_key = api_key
+        self.organization = organization
+
+    def call(self, content, args = {}):
+        # url = "https://api.openai.com/v1/chat/completions"
+        url = "https://43.153.20.180/v1/chat/completions"
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self.api_key}",
+            "OpenAI-Organization": self.organization,
+        }
+        parameters = {
+            "model": 'gpt-3.5-turbo',
+            "messages": [{'role': 'user', 'content': content}],
+            **args,
+        }
+        response = requests.post(
+            url,
+            headers=headers,
+            json=parameters,
+            verify=False
+        )
+        response = json.loads(response.content.decode("utf-8"))
+        if 'error' in response:
+            assert False, str(response)
+        # print(response)
+        return response['choices'][0]['message']['content']
+    
+    
+if __name__=="__main__":
+    gpt=GPT35('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')
+    response=gpt.call('hello')
+    print(response)
