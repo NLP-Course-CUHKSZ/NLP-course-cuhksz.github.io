18ef548a109454bc9646a5a4f5044dddca1c5853
Xidong Wang <73694546+wangxidong06@users.noreply.github.com>
2023-10-07 17:36:07 +0800

    Update gen.py

--- Assignment1_tmp/code/gen.py
+++ Assignment1_tmp/code/gen.py
@@ -1,7 +1,20 @@
 import openai
 
-OPENAI_API_KEY =  ""
+OPENAI_API_KEY =  "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"
 openai.api_key =  OPENAI_API_KEY
 openai.api_base =  "https://openai.huatuogpt.cn/v1"
-chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Hello world"}])
-print(chat_completion)
+response = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "Tell me a fun fact."},
+    {"role": "assistant", "content": "Sure, here is an interesting fact:"}
+  ],
+  temperature=0.6,
+  max_tokens=30,
+  top_p=0.8,
+  frequency_penalty=0.6,
+  presence_penalty=0.8,
+  n=3
+)
+print(response)
