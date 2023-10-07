9143118894cfaeb686b84f8bd4e3cfeafef0161a
Xidong Wang <73694546+wangxidong06@users.noreply.github.com>
2023-10-07 16:18:11 +0800

    Update gen.py

--- Assignment1_tmp/code/gen.py
+++ Assignment1_tmp/code/gen.py
@@ -1,44 +1,7 @@
-import requests
-import json
+import openai
 
-from requests.packages.urllib3.exceptions import InsecureRequestWarning
-
-requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-# from gpt import GPT
-
-class GPT35:
-    def __init__(self,api_key,organization = None) -> None:
-        self.api_key = api_key
-        self.organization = organization
-
-    def call(self, content, args = {}):
-        # url = "https://api.openai.com/v1/chat/completions"
-        url = "https://43.153.20.180/v1/chat/completions"
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization": f"Bearer {self.api_key}",
-            "OpenAI-Organization": self.organization,
-        }
-        parameters = {
-            "model": 'gpt-3.5-turbo',
-            "messages": [{'role': 'user', 'content': content}],
-            **args,
-        }
-        response = requests.post(
-            url,
-            headers=headers,
-            json=parameters,
-            verify=False
-        )
-        response = json.loads(response.content.decode("utf-8"))
-        if 'error' in response:
-            assert False, str(response)
-        # print(response)
-        return response['choices'][0]['message']['content']
-    
-    
-if __name__=="__main__":
-    key="" # API_key
-    gpt=GPT35(key)
-    response=gpt.call('hello')
-    print(response)
+OPENAI_API_KEY =  "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"
+openai.api_key =  OPENAI_API_KEY
+openai.api_base =  "https://openai.huatuogpt.cn/v1"
+chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Hello world"}])
+print(chat_completion)
