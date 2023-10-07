5e3f5dde8431db786832249343f83174738b3610
Xidong Wang <73694546+wangxidong06@users.noreply.github.com>
2023-10-07 16:19:24 +0800

    Update gen.py

--- Assignment1_tmp/code/gen.py
+++ Assignment1_tmp/code/gen.py
@@ -1,6 +1,6 @@
 import openai
 
-OPENAI_API_KEY =  "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"
+OPENAI_API_KEY =  ""
 openai.api_key =  OPENAI_API_KEY
 openai.api_base =  "https://openai.huatuogpt.cn/v1"
 chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Hello world"}])
